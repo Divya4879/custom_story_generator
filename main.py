2428339836dcc1f362c1fd3971c64b995377f4d7
Divya <86043076+Divya4879@users.noreply.github.com>
2024-09-03 02:48:14 +0530

    My iteration of the previous app

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from colour import Color
 from mistralai import Mistral
 
 load_dotenv()
@@ -10,7 +9,7 @@ load_dotenv()
 background_image = """
 <style>
 [data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM7b3QnJ8IX1do_6GkwIO2lXMOjHA2Rxns7frOkEBajW7F6Lnf5wpEJ6j-D07V0637k7U&usqp=CAU");
+    background-image: url("https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTA4L3N0YXJ0dXBpbWFnZXNfYmx1ZV9wdXJwbGVfc2t5X3doaXRlX2Nsb3Vkc19iYWNrZ3JvdW5kX2ltYWdlX2dyYV9jZDNlMTI5My03MjBkLTQ5ZTgtOTZkYy0yMTAyY2RhN2I2MzlfMS5qcGc.jpg");
     background-size: 100vw 100vh;
     background-position: center;  
     background-repeat: no-repeat;
@@ -22,27 +21,33 @@ background_image = """
 st.markdown(background_image, unsafe_allow_html=True)
 
 
-api_key = os.environ.get("MISTRAL_API_KEY")
-model = "mistral-large-latest"
-
-title = '<h1 style="font-family:serif;color:#f2ecfa; font-size: 48px">Your customized story generator</h1>'
+title = '<h1 style="font-family:serif;color:#52327a; font-size: 48px">Your customized story generator</h1>'
 st.markdown(title, unsafe_allow_html=True)
 
-subheading1 = '<h2 style="color:#f5f2f6; font-size: 40px;font-style:italic">Generate a customized story</h1>'
+subheading1 = '<h2 style="color:#52327a; font-size: 40px;font-style:italic">Generate a customized story</h1>'
 st.markdown(subheading1, unsafe_allow_html=True)
 
-text = "Enter the approx no. of words in the story"
 
-words = st.number_input(text, 100, 800, "min", 100)
-
-theme = st.selectbox("Theme: ",
-                     ['Romance', 'Horror', 'Adventurous','Fantasy','Fanfiction'])
+text = f":blue[Enter the approx no. of words in the story]"
+words = st.number_input(text, 400, 1500, "min", 400)
 
 
-genre = st.text_input("Enter the genre",value=None,placeholder="Happy/Tragic/Hopeful Ending, Plot-twist....")
+t = n = f":blue[Theme]"
+theme = st.selectbox(t,
+                     ['Romance', 'Horror', 'Adventurous','Fantasy','Fanfiction','Angst'])
+
+
+
+g = f":blue[Enter the genre]"
+genre = st.text_input(g,value=None,placeholder="Happy/Tragic/Hopeful Ending, Plot-twist....")
 
 if theme== 'Fanfiction':
-    reference = st.text_input("Enter the name of the book/movie/drama on which you want it based on: ",)
+    r = f":blue[Enter the name of the book/movie/drama on which you want it based on]"
+    reference = st.text_input(r)
+
+
+api_key = os.environ.get("MISTRAL_API_KEY")
+model = "mistral-large-latest"
 
 client = Mistral(api_key=api_key)
 
@@ -57,6 +62,7 @@ else:
         model=model,
         messages=[{"role":"system", "content":f"You're a world renowned author. Write a story in about {words} words on the theme of {theme}, also it should have the genre of {genre}."}]
     )
+
 
 
 if st.button("Generate my story"):
